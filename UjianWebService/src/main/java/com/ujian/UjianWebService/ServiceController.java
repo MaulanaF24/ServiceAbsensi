b5886dfe4b3c9f2224d2819893277ed90661b193
codehelpmesleep <project.lana97@gmail.com>
2019-08-26 14:33:46 +0700

    Init

---
+++ UjianWebService/src/main/java/com/ujian/UjianWebService/ServiceController.java
@@ -0,0 +1,28 @@
+package com.ujian.UjianWebService;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ujian.UjianWebService.controller.Controller;
+import com.ujian.UjianWebService.model.User;
+
+
+
+@RestController
+public class ServiceController {
+
+
+	@GetMapping(path = "absensi/user/all")
+
+	public List<User> getAllUser() {
+
+		return Controller.getInstance().getAllUser();
+
+	}
+
+}
---
+++ UjianWebService/src/main/java/com/ujian/UjianWebService/model/User.java
@@ -0,0 +1,70 @@
+package com.ujian.UjianWebService.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+
+@Entity
+@Table(name = "user")
+public class User {
+
+	@Id
+	private int id;
+	@Column
+	private String username;
+	@Column
+	private String email;
+	@OneToMany(mappedBy = "personel")
+	private List<Personel> personel;
+
+	
+	public User() {
+		this.personel = new ArrayList<Personel>();
+	}
+	public User(int id,int user_id) {
+		this.id = id;
+		this.user_id = user_id;
+	}
+	public List<Personel> getPersonel() {
+		return personel;
+	}
+
+	public void setPersonel(List<Personel> personel) {
+		this.personel = personel;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
